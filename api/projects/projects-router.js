65dc1e57b9a4cfabdd06884f9405cb04bffbcf83
Robert Jank <robert.jank@yahoo.com>
2024-10-17 10:33:09 -0500

    added error handiling router to projects-router.js

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,12 @@
-// Write your "projects" router here!
+const express = require("express");
+const {} = require("./projects-middleware");
+
+const router = express.Route();
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+
+module.exports = router;
